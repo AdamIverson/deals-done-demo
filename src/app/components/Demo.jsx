cdc4eeb82cb6c4c806fbd37ba689e397048784e2
AdamIverson <aciverso@gmail.com>
2023-08-02 08:10:47 -0500

    csv data loading and logging on button click

--- src/app/components/Demo.jsx
+++ src/app/components/Demo.jsx
@@ -1,11 +1,41 @@
-"use client"
+"use client";
 
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import * as d3 from "d3";
 
 function Demo() {
+  const svgRef = useRef();
+  const [data, setData] = useState([5, 10, 15, 20, 25]);
+
+  const getCsvData = () => {
+    d3.csv(
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vRM4Un_tyJOy1eUJnE52-XEK0FoOofiszPAtUYU3JnmpuYDOPrrqv901l6AhLPpYprI4MeAFhnBNnBj/pub?output=csv",
+      (d) => {
+        // console.log(d)
+        // (d.number_of_reviews = +d.number_of_reviews), (d.price = +d.price);
+        return d;
+      }
+    )
+      .then((response) => {
+        console.log(response);
+        setData(response);
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+    
+  });
+
   return (
-    <div>Demo</div>
-  )
+    <div>
+      <svg ref={svgRef}></svg>
+      <button onClick={getCsvData}>csv data</button>
+    </div>
+  );
 }
 
-export default Demo
+export default Demo;
